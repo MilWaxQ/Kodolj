949d9b89db7272a9549c44073093b84d17a886a0
MilWaxQ <doczimici@gmail.com>
2019-04-13 20:58:49 +0200

    Tudás Felmérés

---
+++ teszt.js
@@ -0,0 +1,23 @@
+    function jelszoEllenorzes() {
+        var jelszo = document.getElementById("jelszoDoboz");
+            if(jelszo.value== "asdfasz") {
+                logoHover()
+                alert("Sikeres Belépés!")
+                return true;
+            }
+            else {
+                alert("Sikertelen Belépés!")
+                return false;
+            }
+    }
+    function logo() {
+        document.getElementById("logo").style.opacity = 1;
+    }
+    function logoHover() {
+        document.getElementById("logo").style.transform = "scale(1.5)";
+        document.getElementById("logo").style.opacity = 0;
+    }
+    function logoRestore() {
+        document.getElementById("logo").style.opacity = 1;
+        document.getElementById("logo").style.transform = "scale(1)";
+    }
